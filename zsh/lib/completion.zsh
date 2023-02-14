b3ed886a08079182dc738a86709ad3e705287e65
Gavin St. Ours <gavin@st-ours.net>
2023-02-13 21:42:00 -0500

    Deactivate completion conf for now

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -1,2 +1,6 @@
+# Configure completion cache.
+# zstyle ':completion::complete:*' use-cache yes                              # Enable cache for completions.
+# zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache" # Configure completion cache path.
+
 # Location for completions
-zcompdump="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/.zcompdump"
+# zcompdump="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/.zcompdump"
