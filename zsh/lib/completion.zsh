7f32ec8bb3be7c08a3333cc38e699363bf0978ad
Gavin St. Ours <gavin@st-ours.net>
2023-02-14 09:50:52 -0500

    Move zcompdump files out of repo

--- zsh/lib/completion.zsh
+++ zsh/lib/completion.zsh
@@ -1,6 +1,8 @@
 # Configure completion cache.
-# zstyle ':completion::complete:*' use-cache yes                              # Enable cache for completions.
-# zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache" # Configure completion cache path.
+zstyle ':completion::complete:*' use-cache yes                              # Enable cache for completions.
+zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache" # Configure completion cache path.
 
 # Location for completions
-# zcompdump="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/.zcompdump"
+autoload -Uz compinit
+zcompdump="${XDG_CACHE_HOME:-${HOME}/.cache}/zsh/.zcompdump"
+compinit -d $zcompdump
