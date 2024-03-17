2ea8267c7c1be8e2a63b385eea7a241826abaae7
Gavin St. Ours <gavin@st-ours.net>
2024-03-17 08:56:30 -0400

    replace exa with eza

--- zsh/aliases/general.zsh
+++ zsh/aliases/general.zsh
@@ -4,9 +4,9 @@ command_exists () {
   hash "$1" 2> /dev/null
 }
 
-# If exa installed, then use exa for some ls commands
-# if command_exists exa ; then
-    alias l='exa -aF --icons' # Quick ls
-    alias la='exa -aF --icons' # List all
-    alias ll='exa -laF --icons' # Show details
-# fi
+# If eza installed, then use eza for some ls commands
+if command_exists eza ; then
+    alias l='eza -aF --icons' # Quick ls
+    alias la='eza -aF --icons' # List all
+    alias ll='eza -laF --icons' # Show details
+fi
